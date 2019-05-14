1a672c0c20b320286d2c26e2f7fd8e1f75bd8a2d
Ubuntu <ubuntu@ip-172-31-29-229.us-west-2.compute.internal>
2019-05-14 20:01:14 +0000

    move files

---
+++ nab/Data/get_trans_DF.py
@@ -0,0 +1,66 @@
+# This file reads in the raw transcript data, organizes it into a pandas DF, then saves
+# it as a CSV
+
+import numpy as np
+import pandas as pd
+import os
+
+rootdir = os.getcwd() + '/RawTranscripts'
+trans_DF = pd.DataFrame()
+for filename in os.listdir(rootdir):
+    f = open(rootdir + '/' + filename)
+    text = f.read().split()
+    text[:] = [i for i in text if i != '.']
+    text.pop(0)
+    labels = []
+    words = []
+    for i in range(int(len(text)/2)):
+        label = text[i*2][text[i*2].find('/') + 1: text[i*2].find('.lab')]
+        word = text[i*2+1].lower()
+        labels.append(label)
+        words.append(word)
+    trans_DF_temp = pd.DataFrame(data = {'label': labels, 'word': words})
+    splits = trans_DF_temp.label.str.split('_', expand = True)
+    trans_DF_temp = trans_DF_temp.assign(P1 = splits[0], P2 = splits[1], P3 = splits[2], P4 = splits[3],
+                          control = np.where(trans_DF_temp.label.str[0] == 'C', 1, 0))
+    trans_DF_temp = trans_DF_temp.assign(gender = np.where((trans_DF_temp.P1.str[0] == 'M') | (trans_DF_temp.P1.str[1] == 'M'), 'M', 'F'))
+    trans_DF = trans_DF.append(trans_DF_temp)
+trans_DF = trans_DF.sort_values(['P1','P2','P3','P4']).reset_index(drop = True)
+cols = list(trans_DF)
+cols.append(cols.pop(cols.index('word')))
+trans_DF = trans_DF[cols]
+
+# split into train/dev/test
+np.random.seed(100)
+trans_DF = trans_DF.assign(train = np.where((trans_DF.P1 == 'F04') | (trans_DF.P1 ==
+                            'M14') | (trans_DF.P1 == 'M16') | (trans_DF.P1 == 'CM13'), 0,
+                            1))
+devind = np.random.rand(trans_DF.shape[0])
+trans_DF = trans_DF.assign(dev = np.where((devind <= 0.6) & (trans_DF.train == 0) & (
+    trans_DF.P1 != 'CM13'), 1, 0))
+trans_DF = trans_DF.assign(test = np.where((trans_DF.train == 0) & (trans_DF.dev == 0), 1,
+                                           0))
+
+# match data to associated file, add column for it
+trainfiles = os.listdir(os.getcwd() + '/audio/train')
+devfiles = os.listdir(os.getcwd() + '/audio/dev')
+testfiles = os.listdir(os.getcwd() + '/audio/test')
+
+trans_DF = trans_DF.assign(fileloc = "NA")
+
+print('size before: ', trans_DF.shape)
+for i in range(trans_DF.shape[0]):
+    if (trans_DF.loc[i, 'train'] == 1):
+        if ((trans_DF.loc[i, 'label'] + '.wav') in trainfiles):
+            trans_DF.loc[i, 'fileloc'] = trans_DF.loc[i, 'label'] + '.wav'
+    if (trans_DF.loc[i, 'dev'] == 1):
+        if ((trans_DF.loc[i, 'label'] + '.wav') in devfiles):
+            trans_DF.loc[i, 'fileloc'] = trans_DF.loc[i, 'label'] + '.wav'
+    if (trans_DF.loc[i, 'test'] == 1):
+        if ((trans_DF.loc[i, 'label'] + '.wav') in testfiles):
+            trans_DF.loc[i, 'fileloc'] = trans_DF.loc[i, 'label'] + '.wav'
+
+trans_DF = trans_DF[trans_DF.fileloc != 'NA'].reset_index(drop = True)
+print('size after: ', trans_DF.shape)
+
+trans_DF.to_csv(os.getcwd() + '/trans_DF.csv')
---
+++ nab/Data/split_data.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+
+dirname = os.getcwd() + '/audio'
+folders = [folder for folder in os.listdir(dirname)]
+folders.remove('train')
+folders.remove('dev')
+folders.remove('test')
+
+testfolders = ['CM13', 'F04', 'M14', 'M16']
+trainfolders = [i for i in folders if i not in testfolders]
+
+# get transcriptDF
+trans_DF = pd.read_csv(os.getcwd() + '/trans_DF.csv')
+
+for folder in trainfolders:
+
+    wavefiles = [f for f in os.listdir(dirname + '/' + folder) if f[-4:] == '.wav']
+
+    for wavefile in wavefiles:
+        os.rename(dirname + '/' + folder + '/' + wavefile, dirname + '/train/' + wavefile)
+
+for folder in testfolders:
+    wavefiles = [f for f in os.listdir(dirname + '/' + folder) if f[-4:] == '.wav']
+
+    for wavefile in wavefiles:
+        vals = trans_DF.loc[trans_DF.label == wavefile[0:-4], 'test'].values
+        if vals.size == 0:
+            os.rename(dirname + '/' + folder + '/' + wavefile,
+                      dirname + '/dev/' + wavefile)
+        elif vals[0] == 1:
+            os.rename(dirname + '/' + folder + '/' + wavefile,
+                      dirname + '/test/' + wavefile)
+        else:
+            os.rename(dirname + '/' + folder + '/' + wavefile,
+                      dirname + '/dev/' + wavefile)
