bb6a960f2d30accb66a503da8447d5510939bb83
jtcampbellsoup <jeremytate.campbell@gmail.com>
2019-05-13 19:19:32 -0700

    match wav and transcript

--- Data/temp.py
+++ Data/temp.py
@@ -1,16 +1,28 @@
 import os
 import pandas as pd
 
-folder = os.getcwd() + '/audio/dev'
+trainfolder = os.getcwd() + '/audio/train'
+devfolder = os.getcwd() + '/audio/dev'
+testfolder = os.getcwd() + '/audio/test'
 
 # get transcriptDF
 trans_DF = pd.read_csv(os.getcwd() + '/trans_DF.csv')
 
-wavefiles = [f for f in os.listdir(folder) if f[-4:] == '.wav']
+trainwavefiles = [f for f in os.listdir(trainfolder) if f[-4:] == '.wav']
+devwavefiles = [f for f in os.listdir(devfolder) if f[-4:] == '.wav']
+testwavefiles = [f for f in os.listdir(testfolder) if f[-4:] == '.wav']
 
-for wavefile in wavefiles:
-    vals = trans_DF.loc[trans_DF.label == wavefile[0:-4], 'test'].values
-    if vals.size != 0:
-        if vals[0] == 1:
-            os.rename(folder + '/' + wavefile,
-                      os.getcwd() + '/audio/test/' + wavefile)
+count = 0
+for wavefile in trainwavefiles:
+    match = trans_DF.loc[trans_DF.label == wavefile[0:-4]]
+    if match.shape[0] == 1:
+        count += 1
+for wavefile in devwavefiles:
+    match = trans_DF.loc[trans_DF.label == wavefile[0:-4]]
+    if match.shape[0] == 1:
+        count += 1
+for wavefile in testwavefiles:
+    match = trans_DF.loc[trans_DF.label == wavefile[0:-4]]
+    if match.shape[0] == 1:
+        count += 1
+print(count)
